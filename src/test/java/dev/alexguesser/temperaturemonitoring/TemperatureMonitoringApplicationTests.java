b0b1510fe5cc26b7ef9f30d7d76d9036bf460750
Axonify-Alex-Guesser <140539263+Axonify-Alex-Guesser@users.noreply.github.com>
2025-05-19 11:13:43 -0300

    Checking git updates

---
+++ src/test/java/dev/alexguesser/temperaturemonitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexguesser.temperaturemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
